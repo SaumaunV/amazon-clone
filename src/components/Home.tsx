5f014e016655c93b9bb5df6ed830619c22e03477
Saumaun <svahedipour1@gmail.com>
2022-04-25 17:59:43 -0500

    added products to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,54 @@
 import React from 'react'
 import styled from 'styled-components'
+import Product from './Product';
 
 function Home() {
   return (
     <Container>
       <img
+        className="homeImage"
         src="https://m.media-amazon.com/images/I/716kWBue3fL._SX3000_.jpg"
         alt="Home page"
       />
+
+      <ProductWrapper>
+        <Product
+          title="All-new Echo Dot (4th Gen, 2020 release) | Smart speaker with Alexa | Charcoal"
+          price={29.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/714Rq4k05UL._AC_SL1000_.jpg"
+          rating={5}
+        />
+        <Product
+          title="LG 27GL83A-B 27 Inch Ultragear QHD IPS 1ms NVIDIA G-SYNC Compatible Gaming Monitor, Black"
+          price={379.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/91lS5yEDH-L._AC_SL1500_.jpg"
+          rating={5}
+        />
+        <Product
+          title="Bose QuietComfort 35 II Wireless Bluetooth Headphones, Noise-Cancelling, with Alexa Voice Control -Silver"
+          price={269.0}
+          image="https://images-na.ssl-images-amazon.com/images/I/71%2BiQZU-dVL._AC_SL1500_.jpg"
+          rating={5}
+        />
+        <Product
+          title="TP-Link AC1750 Smart WiFi Router (Archer A7) - Dual Band Gigabit Wireless Internet Router for Home, Works with Alexa, VPN Server, Parental Control and QoS"
+          price={56.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/51R2a9p-vNL._AC_SL1000_.jpg"
+          rating={5}
+        />
+        <Product
+          title="eufy by Anker, BoostIQ RoboVac 11S (Slim), Robot Vacuum Cleaner, Super-Thin, 1300Pa Strong Suction, Quiet, Self-Charging Robotic Vacuum Cleaner, Cleans Hard Floors to Medium-Pile Carpets"
+          price={149.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/71TmTj7HjNL._AC_SL1500_.jpg"
+          rating={5}
+        />
+        <Product
+          title="Sabrent 4-Port USB 2.0 Hub with Individual LED lit Power Switches (HB-UMLS)"
+          price={7.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/71igbmDJwDL._AC_SL1500_.jpg"
+          rating={5}
+        />
+      </ProductWrapper>
     </Container>
   );
 }
@@ -15,12 +56,22 @@ function Home() {
 export default Home
 
 const Container = styled.div`
-  height: 100vh;
+  height: 100%;
   background-color: #eaeded;
 
-  img {
+  .homeImage {
     height: 600px;
+    z-index: -1;
     margin-bottom: -150px;
     mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
   }
 `;
+
+const ProductWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  column-gap: 40px;
+  row-gap: 40px;
+  padding-bottom: 40px;
+`
---
+++ src/components/Product.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface Props {
+    title: string,
+    price: number,
+    image: string,
+    rating: number  
+}
+
+function Product(props: Props) {
+  return (
+    <Container>
+      <h3>{props.title}</h3>
+
+      <ProductRating>
+        {Array(props.rating)
+          .fill(null)
+          .map((_, index) => (
+            <p key={index}>★</p>
+          ))}
+      </ProductRating>
+
+      <p>
+        <small>$</small>
+        <span>{props.price}</span>
+      </p>
+
+      <div className="productImage">
+        <img src={props.image} alt={props.title} />
+      </div>
+
+      <Button>
+        <button>Add to Cart</button>
+      </Button>
+    </Container>
+  );
+}
+
+export default Product
+
+const Container = styled.div`
+  height: 450px;
+  width: 700px;
+  background-color: white;
+  padding: 20px;
+  text-align: left;
+  z-index: 1;
+
+  .productImage {
+    text-align: center;
+  }
+
+  img {
+    height: 200px;
+  }
+
+  p {
+    font-size: 20px;
+  }
+
+  small {
+    vertical-align: text-top;
+  }
+`;
+
+const ProductRating = styled.div`
+  display: flex;
+
+  p {
+    color: gold;
+  }
+`
+
+const Button = styled.div`
+  margin-top: 30px;
+  text-align: center;
+
+  button {
+    height: 30px;
+    width: 200px;
+    border-radius: 20px;
+    border: none;
+    background-color: rgb(255, 216, 20);
+    border: 1px solid rgb(252, 210, 0);
+    cursor: pointer;
+
+    &:hover {
+      background-color: #f7ca00;
+      border-color: #f2c200;
+    }
+  }
+`
