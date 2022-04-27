dc8af8bc467ff527e6a57fb90cc9e7eb46e0aa7f
Saumaun <svahedipour1@gmail.com>
2022-04-27 17:53:13 -0500

    created shopping cart page

---
+++ src/components/Cart.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+function Cart() {
+  return (
+    <Container>
+      <ShoppingCart>
+        <h1>Shopping Cart</h1>
+        <hr />
+      </ShoppingCart>
+    </Container>
+  );
+}
+
+export default Cart;
+
+const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    background-color: #eaeded;
+    padding: 14px 18px;
+`;
+
+const ShoppingCart = styled.div`
+    height: 100%;
+    width: 1500px;
+    background-color: white;
+    margin-bottom: 20px;
+
+    h1 {
+        margin-bottom: 20px;
+        padding-left: 20px;
+        padding-top: 20px;
+        font-weight: 400;
+        text-align: left;
+    }
+
+    hr {
+        margin: 0 20px;
+    }
+`
