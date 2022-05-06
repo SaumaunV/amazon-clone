6b19ceab64bf79536af6bc18a842ecf1739af322
Saumaun <svahedipour1@gmail.com>
2022-05-06 00:33:38 -0500

    changed to react 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import StateProvider from './context/StateProvider';
 import reducer, { initialState } from './context/reducer';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = document.getElementById("root");
+ReactDOM.render(
   <React.StrictMode>
     <StateProvider reducer={reducer} initialState={initialState}>
       <App />
     </StateProvider>
-  </React.StrictMode>
+  </React.StrictMode>, root
 );
